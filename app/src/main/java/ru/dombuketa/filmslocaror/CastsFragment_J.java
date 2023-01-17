ade4d037df923803b3b718c647820dfffd3e70d4
fafa <fafa@chat.ru>
2023-01-17 18:03:02 +0300

    add_J

---
+++ app/src/main/java/ru/dombuketa/filmslocaror/CastsFragment_J.java
@@ -0,0 +1,32 @@
+package ru.dombuketa.filmslocaror;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.Fragment;
+
+public class CastsFragment_J extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_casts, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ConstraintLayout casts_fragment_root = requireActivity().findViewById(R.id.casts_fragment_root);
+        AnimationHelper_J.performFragmentCircularRevealAnimation(casts_fragment_root, requireActivity(),4);
+
+    }
+}
