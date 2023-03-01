92be02e1d569fd82ace3afa771b4f1a3e0e0ddf9
fafa <fafa@chat.ru>
2023-03-01 15:50:06 +0300

    completed with @Binds (DatabaseModule)

--- app/src/main/java/ru/dombuketa/filmslocaror/data/MainRepository.kt
+++ app/src/main/java/ru/dombuketa/filmslocaror/data/MainRepository.kt
@@ -2,7 +2,8 @@ package ru.dombuketa.filmslocaror.data
 
 import ru.dombuketa.filmslocaror.R
 import ru.dombuketa.filmslocaror.domain.Film
+import javax.inject.Inject
 
-class MainRepository {
-    val filmsDataBase = ArrayList<Film>()
+class MainRepository @Inject constructor() : IRepository {
+    override val filmsDataBase = ArrayList<Film>()
 }
--- app/src/main/java/ru/dombuketa/filmslocaror/di/modules/DatabaseModule.kt
+++ app/src/main/java/ru/dombuketa/filmslocaror/di/modules/DatabaseModule.kt
@@ -1,13 +1,14 @@
 package ru.dombuketa.filmslocaror.di.modules
 
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
+import ru.dombuketa.filmslocaror.data.IRepository
 import ru.dombuketa.filmslocaror.data.MainRepository
 import javax.inject.Singleton
 
 @Module
-class DatabaseModule {
-    @Provides
-    @Singleton
-    fun provideRepository() = MainRepository()
+abstract class DatabaseModule{
+    @Binds
+    abstract fun bindRepository(mainRepository: MainRepository) : IRepository
 }
